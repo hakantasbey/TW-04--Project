51cd3db76584fb66c78782b2e3f01f3fa8986e43
hakan <tasbeyhakan@gmail.com>
2023-10-21 16:02:42 +0300

    first-commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// let myArray = [1, 2, 3, 4, 5];
+
+// myArray.splice(2,1,6)
+// console.log(myArray);
+
+// const numbers = [0, 1, 2, 3, 4, 5];
+
+// const evenNumbers = numbers.filter((number)=> !(number%2))
+// console.log(evenNumbers);
+
+
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const sum = numbers.reduce((acc, num)=> acc+num,0)
+
+// console.log(sum);
+
+// let str = "Once a day";
+// let index = str.indexOf("a");
+// let index2 = str.lastIndexOf("a");
+// console.log(str.indexOf("a", index + 1) === index2);
+
+
